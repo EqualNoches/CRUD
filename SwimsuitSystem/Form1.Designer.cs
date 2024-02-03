df38d1b33f3e75baa81c6fa0ffa71a583f3def23
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2024-02-03 00:53:05 -0400

    Pequeños Avances
    Avances en el Ui e inicio en la logica de programacion

--- SwimsuitSystem/Form1.Designer.cs
+++ SwimsuitSystem/Form1.Designer.cs
@@ -28,12 +28,216 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            lblName = new Label();
+            lblLastName = new Label();
+            lblGender = new Label();
+            lblBirthday = new Label();
+            lblNationality = new Label();
+            lblPhoneNumber = new Label();
+            lblEmail = new Label();
+            txbFirstName = new TextBox();
+            txbLastName = new TextBox();
+            dtpBirthday = new DateTimePicker();
+            rdbMale = new RadioButton();
+            rdbFemale = new RadioButton();
+            txbNationality = new TextBox();
+            txbPhoneNumber = new TextBox();
+            txbEmail = new TextBox();
+            btnSubmit = new Button();
+            SuspendLayout();
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(40, 74);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(144, 37);
+            lblName.TabIndex = 0;
+            lblName.Text = "First Name";
+            lblName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblLastName
+            // 
+            lblLastName.AutoSize = true;
+            lblLastName.Location = new Point(40, 184);
+            lblLastName.Name = "lblLastName";
+            lblLastName.Size = new Size(142, 37);
+            lblLastName.TabIndex = 1;
+            lblLastName.Text = "Last Name";
+            lblLastName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblGender
+            // 
+            lblGender.AutoSize = true;
+            lblGender.Location = new Point(40, 289);
+            lblGender.Name = "lblGender";
+            lblGender.Size = new Size(104, 37);
+            lblGender.TabIndex = 3;
+            lblGender.Text = "Gender";
+            lblGender.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblBirthday
+            // 
+            lblBirthday.AutoSize = true;
+            lblBirthday.Location = new Point(40, 412);
+            lblBirthday.Name = "lblBirthday";
+            lblBirthday.Size = new Size(115, 37);
+            lblBirthday.TabIndex = 4;
+            lblBirthday.Text = "Birthday";
+            lblBirthday.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblNationality
+            // 
+            lblNationality.AutoSize = true;
+            lblNationality.Location = new Point(34, 552);
+            lblNationality.Name = "lblNationality";
+            lblNationality.Size = new Size(148, 37);
+            lblNationality.TabIndex = 5;
+            lblNationality.Text = "Nationality";
+            lblNationality.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblPhoneNumber
+            // 
+            lblPhoneNumber.AutoSize = true;
+            lblPhoneNumber.Location = new Point(34, 681);
+            lblPhoneNumber.Name = "lblPhoneNumber";
+            lblPhoneNumber.Size = new Size(196, 37);
+            lblPhoneNumber.TabIndex = 8;
+            lblPhoneNumber.Text = "Phone Number";
+            lblPhoneNumber.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblEmail
+            // 
+            lblEmail.AutoSize = true;
+            lblEmail.Location = new Point(40, 816);
+            lblEmail.Name = "lblEmail";
+            lblEmail.Size = new Size(82, 37);
+            lblEmail.TabIndex = 9;
+            lblEmail.Text = "Email";
+            lblEmail.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // txbFirstName
+            // 
+            txbFirstName.Location = new Point(250, 74);
+            txbFirstName.Name = "txbFirstName";
+            txbFirstName.Size = new Size(313, 43);
+            txbFirstName.TabIndex = 10;
+            txbFirstName.TextChanged += txbFirstName_TextChanged;
+            // 
+            // txbLastName
+            // 
+            txbLastName.Location = new Point(250, 184);
+            txbLastName.Name = "txbLastName";
+            txbLastName.Size = new Size(313, 43);
+            txbLastName.TabIndex = 11;
+            txbLastName.TextChanged += txbLastName_TextChanged;
+            // 
+            // dtpBirthday
+            // 
+            dtpBirthday.Location = new Point(250, 412);
+            dtpBirthday.Name = "dtpBirthday";
+            dtpBirthday.Size = new Size(441, 43);
+            dtpBirthday.TabIndex = 12;
+            // 
+            // rdbMale
+            // 
+            rdbMale.AutoSize = true;
+            rdbMale.Location = new Point(250, 287);
+            rdbMale.Name = "rdbMale";
+            rdbMale.Size = new Size(107, 41);
+            rdbMale.TabIndex = 13;
+            rdbMale.TabStop = true;
+            rdbMale.Text = "Male";
+            rdbMale.UseVisualStyleBackColor = true;
+            // 
+            // rdbFemale
+            // 
+            rdbFemale.AutoSize = true;
+            rdbFemale.Location = new Point(430, 289);
+            rdbFemale.Name = "rdbFemale";
+            rdbFemale.Size = new Size(133, 41);
+            rdbFemale.TabIndex = 14;
+            rdbFemale.TabStop = true;
+            rdbFemale.Text = "Female";
+            rdbFemale.UseVisualStyleBackColor = true;
+            // 
+            // txbNationality
+            // 
+            txbNationality.Location = new Point(250, 552);
+            txbNationality.Name = "txbNationality";
+            txbNationality.Size = new Size(313, 43);
+            txbNationality.TabIndex = 15;
+            txbNationality.TextChanged += txbNationality_TextChanged;
+            // 
+            // txbPhoneNumber
+            // 
+            txbPhoneNumber.Location = new Point(250, 681);
+            txbPhoneNumber.Name = "txbPhoneNumber";
+            txbPhoneNumber.Size = new Size(313, 43);
+            txbPhoneNumber.TabIndex = 17;
+            txbPhoneNumber.TextChanged += txbPhoneNumber_TextChanged;
+            // 
+            // txbEmail
+            // 
+            txbEmail.Location = new Point(250, 816);
+            txbEmail.Name = "txbEmail";
+            txbEmail.Size = new Size(313, 43);
+            txbEmail.TabIndex = 18;
+            txbEmail.TextChanged += txbEmail_TextChanged;
+            // 
+            // btnSubmit
+            // 
+            btnSubmit.Location = new Point(407, 962);
+            btnSubmit.Name = "btnSubmit";
+            btnSubmit.Size = new Size(284, 109);
+            btnSubmit.TabIndex = 19;
+            btnSubmit.Text = "Submit";
+            btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(15F, 37F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1034, 1235);
+            Controls.Add(btnSubmit);
+            Controls.Add(txbEmail);
+            Controls.Add(txbPhoneNumber);
+            Controls.Add(txbNationality);
+            Controls.Add(rdbFemale);
+            Controls.Add(rdbMale);
+            Controls.Add(dtpBirthday);
+            Controls.Add(txbLastName);
+            Controls.Add(txbFirstName);
+            Controls.Add(lblEmail);
+            Controls.Add(lblPhoneNumber);
+            Controls.Add(lblNationality);
+            Controls.Add(lblBirthday);
+            Controls.Add(lblGender);
+            Controls.Add(lblLastName);
+            Controls.Add(lblName);
+            Name = "Form1";
+            Text = "Swim Suit";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblName;
+        private Label lblLastName;
+        private Label lblGender;
+        private Label lblBirthday;
+        private Label lblNationality;
+        private Label lblPhoneNumber;
+        private Label lblEmail;
+        private TextBox txbFirstName;
+        private TextBox txbLastName;
+        private DateTimePicker dtpBirthday;
+        private RadioButton rdbMale;
+        private RadioButton rdbFemale;
+        private TextBox txbNationality;
+        private TextBox txbPhoneNumber;
+        private TextBox txbEmail;
+        private Button btnSubmit;
     }
 }
--- SwimsuitSystem/Form1.cs
+++ SwimsuitSystem/Form1.cs
@@ -1,10 +1,67 @@
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
+
 namespace SwimsuitSystem
 {
     public partial class Form1 : Form
     {
+        //patron regex para saber si solo se encuentran letras en una cadena
+        string regexPattern = "^[a-zA-Z]+$";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // Esta funcion se encarga de asegurar que solo hayan letras dentro de un textbox
+        public void ValidFormat(string format, TextBox txb)
+        {
+            bool matches = Regex.IsMatch(format, regexPattern);
+            if (!matches)
+            {
+                MessageBox.Show("Please enter a valid input", "Incorrect format", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txb.Clear();
+            }
+        }
+
+        private void txbFirstName_TextChanged(object sender, EventArgs e)
+        {
+            string name = txbFirstName.Text;
+            ValidFormat(name, txbFirstName);
+
+        }
+
+        private void txbLastName_TextChanged(object sender, EventArgs e)
+        {
+            string lastName = txbLastName.Text;
+            ValidFormat(lastName, txbLastName);
+        }
+
+        private void txbNationality_TextChanged(object sender, EventArgs e)
+        {
+            string nationality = txbNationality.Text;
+            ValidFormat(nationality, txbNationality);
+        }
+
+        private void txbPhoneNumber_TextChanged(object sender, EventArgs e)
+        {
+            string phoneNumber = txbPhoneNumber.Text;
+
+            // validar si es esta escribiendo un numero de telefono
+            string regexPatternPhoneNumber = "^\\s?(?:\\d{3}-\\d{3}-\\d{4}|1 \\d{3}-\\d{3}-\\d{4})\\s?$";
+            if (Regex.IsMatch(phoneNumber, regexPatternPhoneNumber)) 
+            {
+                MessageBox.Show("Please enter a valid input", "Incorrect format", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txbPhoneNumber.Clear();
+            }
+        }
+
+        private void txbEmail_TextChanged(object sender, EventArgs e)
+        {
+            string email = txbEmail.Text;
+            ValidFormat(email, txbEmail);
         }
     }
 }
